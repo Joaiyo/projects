a1544a0c4ab2b8e92894852b0c6b50d64043b2f7
Jovo <jolenevoong@gmail.com>
2024-10-29 12:32:53 -0700

    Create TriviaMain.java

---
+++ Trivia Game/TriviaMain.java
@@ -0,0 +1,11 @@
+/*
+ * Date: 12/6/2023
+ * Description: This program creates a simple trivia game that uses arrays to store the questions and answers.
+ */ 
+
+public class TriviaMain {
+    
+    public static void main(String[] args) {
+        TriviaHelper.run();
+    }
+}
