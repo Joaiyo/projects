5d137f9f2b3036a3ba20e2ddf1c986bee6bb1ce4
Jovo <jolenevoong@gmail.com>
2024-10-29 12:47:30 -0700

    Create GuessTheNumberMain.java

---
+++ Guess The Number/GuessTheNumberMain.java
@@ -0,0 +1,10 @@
+/*
+ * Date: 10/27/2023
+ * Description: A game where the user tries to guess the correct number picked by the computer.
+ */ 
+
+public class GuessTheNumberMain {
+    public static void main(String[] args) {
+        GuessTheNumberHelper.playGame();
+    }
+}
